cd506d876efb2a53f4f285a47b24315b3b5dbf01
Mobin <wwwfarimancom34@gmail.com>
2023-08-27 01:02:33 +0430

    Commit Base of project

---
+++ app/src/main/java/mobin/shabanifar/foodpart/MainActivity.kt
@@ -0,0 +1,15 @@
+package mobin.shabanifar.foodpart
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
+
