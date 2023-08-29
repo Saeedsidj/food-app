0163b342d8b00445dfd04c7f5e1bcc6a813dc92d
saeed.gholami <saeedsidjcr@gmail.com>
2023-08-29 10:26:13 +0330

    splash api created and committed

--- app/src/main/java/mobin/shabanifar/foodpart/MainActivity.kt
+++ app/src/main/java/mobin/shabanifar/foodpart/MainActivity.kt
@@ -3,10 +3,13 @@ package mobin.shabanifar.foodpart
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
+
         setContent {
 
         }
