26c79690f77b51472f707e98fd52d5672167f4bc
mohammadreza.ghavidel <mohammadreza.ghavidel@partsoftware.com>
2023-09-01 17:13:26 +0330

    #9 home search screen

--- app/src/main/java/mobin/shabanifar/foodpart/screens/search.kt
+++ app/src/main/java/mobin/shabanifar/foodpart/screens/search.kt
@@ -1,8 +1,226 @@
 package mobin.shabanifar.foodpart.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Arrangement.Center
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.Scaffold
+import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import mobin.shabanifar.foodpart.FakeFoods
+import mobin.shabanifar.foodpart.R
+import mobin.shabanifar.foodpart.fakeFoodItems
 
+
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun Search() {
+    val keyboardController = LocalSoftwareKeyboardController.current
+    var textField by remember { mutableStateOf(TextFieldValue("")) }
+    val borderSurface = MaterialTheme.colors.surface
+    val borderOnBack = MaterialTheme.colors.onBackground
+    var isWrite by remember { mutableStateOf(false) }
+    var searched by remember { mutableStateOf("") }
+        //var foundItems by rememberSaveable { mutableStateOf(emptyList<FakeFoods>()) }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                backgroundColor = MaterialTheme.colors.background,
+                contentPadding = PaddingValues(horizontal = 16.dp),
+                elevation = 0.dp
+            ) {
+                IconButton(
+                    modifier = Modifier
+                        .align(CenterVertically)
+                        .size(24.dp),
+                    onClick = { /*TODO*/ }
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.KeyboardArrowRight,
+                        contentDescription = "KeyboardArrowRight",
+                        tint = MaterialTheme.colors.onBackground
+                    )
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    //modifier = Modifier.align(CenterVertically),
+                    text = stringResource(id = R.string.what_are_you_looking_for),
+                    color = MaterialTheme.colors.onBackground,
+                    style = MaterialTheme.typography.h2
+                )
+
+            }
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TextField(
+                modifier = Modifier
+                    //.padding(start = 16.dp, end = 16.dp, top = 16.dp)
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .clip(MaterialTheme.shapes.medium)
+                    .background(MaterialTheme.colors.surface)
+                    .border(
+                        1.dp,
+                        if (isWrite) borderOnBack else borderSurface,
+                        MaterialTheme.shapes.medium
+                    ),
+                value = textField,
+                shape = MaterialTheme.shapes.medium,
+                textStyle = MaterialTheme.typography.body1,
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(
+                    onSearch = {
+                        /*searched = textField.text
+                        foundItems = fakeFoodItems.filter() {
+                            it.name == searched
+                        }*/
+                        keyboardController?.hide()
+                    }
+                ),
+                placeholder = {
+                    Text(
+                        text = stringResource(id = R.string.write_here),
+                        style = MaterialTheme.typography.body1,
+                        color = MaterialTheme.colors.onBackground
+                    )
+                },
+                onValueChange = {
+                    textField = it
+                    isWrite = textField != TextFieldValue("")
+                },
+                trailingIcon = {
+                    IconButton(onClick = {
+                        textField = TextFieldValue("")
+                        isWrite = false
+                    }) {
+                        Icon(
+                            imageVector = Icons.Filled.Clear,
+                            contentDescription = ""
+                        )
+                    }
+                }
+            )
+            Text(text = textField.text)//searched"سیب زمینی"
+
+            //SearchedItems(foundItems)
+            SearchedItems(textField.text)
+
+
+        }
+    }
+}
+
+
+@Composable
+fun SearchedItems(searchValue: String/*foundItems:List<FakeFoods>*/) {
+
+ /*   val foundItems by rememberSaveable {
+        mutableStateOf(
+            fakeFoodItems.filter() {
+                it.name == searchValue
+            }
+        )
+    }*/
+    /*    val foundItems = rememberSaveable {
+            fakeFoodItems.filter(){
+                it.name == searchValue
+            }
+        }*/
+
+    LazyVerticalGrid(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(start = 40.dp, end = 40.dp),
+        columns = GridCells.Fixed(2),
+        horizontalArrangement = Arrangement.spacedBy(24.dp),
+        //verticalArrangement = Center,
+        contentPadding = PaddingValues(vertical = 16.dp)
+    ) {
+
+        items(fakeFoodItems) {
+            if(it.name == searchValue)
+            Column(
+                modifier = Modifier
+                    .padding(bottom = 24.dp)
+                    .clip(MaterialTheme.shapes.medium)
+                    .clickable { /*TODO*/ }
+            ) {
+                Image(
+                    painter = painterResource(id = it.image),
+                    contentDescription = "",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        //.border(1.dp, Color.Black)
+                        .clip(MaterialTheme.shapes.medium)
+                        .size(width = 240.dp, height = 85.dp)
+                )
+
+                Text(
+                    text = it.name,
+                    style = MaterialTheme.typography.body1,
+                    color = MaterialTheme.colors.onSurface,
+                    modifier = Modifier
+                        .padding(start = 8.dp, bottom = 4.dp)
+                )
+                Text(
+                    text = "زمان : " + "${it.time}" + " دقیقه",
+                    style = MaterialTheme.typography.subtitle1,
+                    color = MaterialTheme.colors.onSurface,
+                    modifier = Modifier
+                        .padding(start = 8.dp)
+                )
+            }
+        }
+    }
 
 }
