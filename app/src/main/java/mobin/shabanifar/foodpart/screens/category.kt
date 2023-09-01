0bc6f168006c9f12fe4f2c24a8c95da0810b5ad1
mohammadreza.ghavidel <mohammadreza.ghavidel@partsoftware.com>
2023-09-01 11:58:22 +0330

    #5 some comments and change place of fake data

--- app/src/main/java/mobin/shabanifar/foodpart/screens/category.kt
+++ app/src/main/java/mobin/shabanifar/foodpart/screens/category.kt
@@ -1,28 +1,23 @@
 package mobin.shabanifar.foodpart.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Chip
 import androidx.compose.material.ChipDefaults.chipColors
@@ -37,7 +32,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -47,16 +41,16 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import mobin.shabanifar.foodpart.R
+import mobin.shabanifar.foodpart.categoryListItems
+import mobin.shabanifar.foodpart.fakeFoodItems
 
 @Composable
 fun Category() {
-
+    // برای نشون دادن صفحه غذایی یافت نشد
     var isFood by remember { mutableStateOf<Boolean>(true) }
-    val lambIsFood = { it: Boolean ->
-        isFood = it
-    }
+    val lambIsFood = { it: Boolean -> isFood = it }
+    //
     Scaffold(
         topBar = {
             TopAppBar(
@@ -75,8 +69,10 @@ fun Category() {
         ) {
             Spacer(modifier = Modifier.height(16.dp))
 
+            // نمایش آیتم ها در بالا صفحه
             CategoryItems(lambIsFood)
 
+            //خط جدا کننده اول
             Divider(
                 modifier = Modifier.padding(start = 16.dp, end = 16.dp),
                 //startIndent = 16.dp,
@@ -84,13 +80,17 @@ fun Category() {
                 color = Color.White
             )
 
+            //ساب کتگوری ها
             SubCategory()
-            //Spacer(modifier = Modifier.height(4.dp))
+
+            // جدا کننده دوم
             Divider(
                 modifier = Modifier.padding(start = 16.dp, end = 16.dp),
                 thickness = 1.dp,
                 color = Color.White
             )
+
+            // برای نمایش لیست غذا ها یا صفحه غذایی نیست
             if (isFood) {
                 FoodItems()
 
@@ -104,35 +104,21 @@ fun Category() {
 
 @Composable
 fun CategoryItems(lambIsFood: (Boolean) -> Unit) {
+    // لیست آیتم های کتگوری که بالای صفحه نمایش داده میشن
     val categoryListItems = remember {
-        mutableStateListOf<CategoryItems>(
-            CategoryItems(name = "آبگوشت", image = R.drawable.abgoosht),
-            CategoryItems(name = "غذا نیست", image = R.drawable.abgoosht),
-            CategoryItems(name = "آبگوشت", image = R.drawable.abgoosht),
-            CategoryItems(name = "آبگوشت", image = R.drawable.abgoosht),
-            CategoryItems(name = "آبگوشت", image = R.drawable.abgoosht),
-            CategoryItems(name = "آبگوشت", image = R.drawable.abgoosht),
-            CategoryItems(name = "آبگوشت", image = R.drawable.abgoosht),
-            CategoryItems(name = "آبگوشت", image = R.drawable.abgoosht),
-            CategoryItems(name = "آبگوشت", image = R.drawable.abgoosht),
-            CategoryItems(name = "آبگوشت", image = R.drawable.abgoosht),
-            CategoryItems(name = "آبگوشت", image = R.drawable.abgoosht),
-            CategoryItems(name = "آبگوشت", image = R.drawable.abgoosht),
-            CategoryItems(name = "آبگوشت", image = R.drawable.abgoosht),
-            CategoryItems(name = "آبگوشت", image = R.drawable.abgoosht),
-            CategoryItems(name = "آبگوشت", image = R.drawable.abgoosht),
-            CategoryItems(name = "آبگوشت", image = R.drawable.abgoosht),
-            CategoryItems(name = "آبگوشت", image = R.drawable.abgoosht),
-        )
+        categoryListItems
     }
+
+    // ذخیره ایندکس کتگوری ای که انتخاب شده است برای رنگی کردن آن
     var indexCategoryClicked by remember { mutableStateOf<Int>(0) }
+
+    // نمایش لیست کتگوری ها
     LazyRow(
         modifier = Modifier
             .fillMaxWidth(),
         contentPadding = PaddingValues(horizontal = 8.dp)
     ) {
         itemsIndexed(categoryListItems) { index, it ->
-
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
@@ -142,16 +128,17 @@ fun CategoryItems(lambIsFood: (Boolean) -> Unit) {
                     .clickable {
                         if (it.name == "غذا نیست") {
                             lambIsFood(false)
-                        }else{
+                        } else {
                             lambIsFood(true)
                         }
-                        indexCategoryClicked = 0
-                        categoryListItems[index] =
-                            categoryListItems[index].copy(isClicked = !it.isClicked)
+                        //indexCategoryClicked = 0
+                        /*categoryListItems[index] =
+                            categoryListItems[index].copy(isClicked = !it.isClicked)*/
                         indexCategoryClicked = index
                     }
             ) {
 
+                // نمایش آیتمی که کلیکشده بصورت رنگی
                 if (index == indexCategoryClicked) {
                     Image(
                         modifier = Modifier
@@ -167,7 +154,9 @@ fun CategoryItems(lambIsFood: (Boolean) -> Unit) {
                         contentScale = ContentScale.Crop,
                         contentDescription = ""
                     )
+                    // فاصله بین عکس کتگوری و نام کتگوری
                     Spacer(modifier = Modifier.height(8.dp))
+
                     Text(
                         modifier = Modifier.fillMaxWidth(),
                         text = it.name,
@@ -192,7 +181,7 @@ fun CategoryItems(lambIsFood: (Boolean) -> Unit) {
                         color = MaterialTheme.colors.onBackground
                     )
                 }
-
+                // ایجاد فاصله بین اسم کتگوری و خط جدا کننده اول
                 Spacer(modifier = Modifier.height(8.dp))
             }
 
@@ -203,33 +192,47 @@ fun CategoryItems(lambIsFood: (Boolean) -> Unit) {
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun SubCategory() {
+    // ذخیره ایندکسی که کلیک شده برای رنگی کردن آن
     var indexSubCategoryClicked by remember { mutableStateOf<Int>(0) }
+
+    // لیست ساب کتگوری ها - لیست استرینگی ساده
     val subCategoryList = remember {
         mutableStateListOf<String>(
-            "hsbd",
-            "djvb",
-            "kshdb",
-            "kshvbhmsv",
-            "aldjvbksd",
-            "kahvbk",
-            "ljbdd",
-            "kahbjhasv",
-            "kugakhb"
+            "آبگوشت",
+            "دیزی",
+            "اشکنه",
+            "آش",
+            "آبگوشت",
+            "دیزی",
+            "اشکنه",
+            "آش",
+            "آبگوشت",
+            "دیزی",
+            "اشکنه",
+            "آش",
+            "آبگوشت",
+            "دیزی",
+            "اشکنه",
+            "آش",
+
         )
     }
+
     LazyRow(
         contentPadding = PaddingValues(horizontal = 12.dp)
     ) {
         itemsIndexed(subCategoryList) { index, it ->
             Chip(
-                modifier = Modifier.padding(start = 4.dp),
+                modifier = Modifier//.border(1.dp,MaterialTheme.colors.primary,MaterialTheme.shapes.medium)
+                    .padding(start = 4.dp),
+
                 shape = MaterialTheme.shapes.medium,
                 colors = chipColors(
                     backgroundColor = MaterialTheme.colors.surface,
                     contentColor = MaterialTheme.colors.onBackground,
                 ),
                 onClick = {
-                    indexSubCategoryClicked = 0
+                    //indexSubCategoryClicked = 0
                     /*subCategoryList[index] =
                         subCategoryList[index].copy(isCategoryItem = !it.isClicked)*/
                     indexSubCategoryClicked = index
@@ -257,6 +260,12 @@ fun SubCategory() {
 
 @Composable
 fun FoodItems() {
+
+    // فیک لیست فود آیتم
+    var fakeFoodItems = remember {
+        fakeFoodItems
+    }
+
     LazyVerticalGrid(
         modifier = Modifier
             .fillMaxSize()
@@ -266,7 +275,7 @@ fun FoodItems() {
         verticalArrangement = Arrangement.Center,
         contentPadding = PaddingValues(vertical = 16.dp)
     ) {
-        items(fakeItems) {
+        items(fakeFoodItems) {
             Column(
                 modifier = Modifier
                     .padding(bottom = 24.dp)
@@ -340,45 +349,4 @@ fun NoFood() {
         }
     }
 }
-
-
-val itemImage = R.drawable.food_itemj
-
-data class ItemFakeData(val image: Int, val name: String, val time: Int)
-
-val fakeItems = listOf<ItemFakeData>(
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی", time = 15),
-    ItemFakeData(image = itemImage, name = "سیب زمینی2", time = 15),
-)
-
-data class CategoryItems(val name: String, val image: Int, var isClicked: Boolean = false)
 
